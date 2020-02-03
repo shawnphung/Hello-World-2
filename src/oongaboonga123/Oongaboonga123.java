35e99c9e47a57daec8e6bbd474bd8b2965a28910
shphu0697 <shphu0697@7HFM6Y1.Student.UGDSB.ED>
2020-02-03 13:20:03 -0500

    Oonga Boonga. Gotta do stuff

---
+++ src/oongaboonga123/Oongaboonga123.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oongaboonga123;
+
+/**
+ *
+ * @author shphu0697
+ */
+public class Oongaboonga123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i = 1;
+       i = i++;
+        do {
+        System.out.println("Hello World");
+    } while(i < 10000 );
+        }
+    
+}
